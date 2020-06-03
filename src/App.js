8e16c0aab5100d2d9162a126b6ba5d0697d1c7ff
Lindsay W <lwhite5314@gmail.com>
2020-06-03 12:06:57 -0400

    input showing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+const todos = [
+  {
+    task: 'Clean kitchen',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: Date.now(),
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(){
+    super();
+    this.state ={
+        todos: todos
+    };
+  }
+
+  toggleCompleted = todoId=>{
+    this.setState({
+      todos:this.state.todos.map(todo =>{
+        if(todoId ===todo.id){
+          return{
+            ...todo, completed:!todo.completed
+          }  
+        }
+        return todo;
+      })
+    })
+  }
+
+  addTodo=(e,todo)=>{
+    e.preventDefault();
+    const newTodo = {
+      task: todo,
+      id:Date.now(),
+      completed: false
+    }
+    this.setState({
+      todos:[...this.state.todos, newTodo]
+    })
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm addTodo={this.addTodo}/>
+        <TodoList toggleCompleted = {this.toggleCompleted} todos ={this.state.todos}/>
       </div>
     );
   }
---
+++ src/components/TodoForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+class TodoForm extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            todo:""
+        }
+    }
+    handleChanges =e=>{
+        this.setState({todo:e.target.value});
+    };
+    submitTodo=e=>{
+        e.preventDefault();
+        this.props.addTodo(e, this.state.todo);
+    };
+
+    render(){
+        return(
+            <form onSubmit={this.submitTodo}>
+                <input
+                type="text"
+                value={this.state.todo}
+                name="todo"
+                onChange={this.handleChanges}
+                />
+                <button>Enter</button>
+            </form>
+        )
+    }
+};
+export default TodoForm;
+
+
